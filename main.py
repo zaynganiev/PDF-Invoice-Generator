d4907e8cd6bfb93f4be5774934edaa1a0ea0afcc
zaynganiev <84282742+zaynganiev@users.noreply.github.com>
2025-06-01 09:47:42 -0400

    Add header to invoice PDF with formatted column names

--- main.py
+++ main.py
@@ -24,6 +24,19 @@ for filepath in filepaths:
     pdf.cell(w=50, h=8, txt=f"Date: {date}", ln=1)
 
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
+    # Add a header
+    columns = df.columns
+    columns = [item.replace("_", " ").title() for item in columns]
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt=columns[0], border=1)
+    pdf.cell(w=70, h=8, txt=columns[1], border=1)
+    pdf.cell(w=30, h=8, txt=columns[2], border=1)
+    pdf.cell(w=30, h=8, txt=columns[3], border=1)
+    pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
+
+    # Add rows to the table
     for index, row in df.iterrows():
         pdf.set_font(family="Times", size=10)
         pdf.set_text_color(80, 80, 80)
